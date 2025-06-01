9fc8e142dcf730d572ab60378f59226044408190
Shourya Gupta <apid.shourya@gmail.com>
2025-06-01 10:03:53 +0530

    Remove Google Analytics scripts from RootLayout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,21 +24,6 @@ export default function RootLayout({
       suppressHydrationWarning
       className={`${satoshi.variable} ${nibpro.variable}`}
     >
-      <head>
-        {/* Google tag (gtag.js) */}
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-MV7RS8E2K9"
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-MV7RS8E2K9');
-          `}
-        </Script>
-      </head>
       <body>
         <ThemeProvider
           attribute="class"
